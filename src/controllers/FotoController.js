37aca75b014f6035b95bf92ea186a05103197650
leonardo-policarpo <leo.dumont@outlook.com>
2023-09-22 08:32:02 -0300

    salvando fotos no banco de dados

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -11,12 +11,12 @@ const upload = multer(multerConfig).single('foto');
 
 class FotoController {
   // método store pois estamos criando (put)
-  async store(req, res) {
+  store(req, res) {
     // executamos a função upload configurada e que ao ser executada
     // vamos capturar algum possível erro, pra isso, passamos como parametro
     // a requisição e a resposta do pedido, em seguida uma função pra ser executada
     // se houver algum erro como já fizemos
-    return upload(req, res, (error) => {
+    return upload(req, res, async (error) => {
       if (error) {
         return res.status(400).json({
           // caso haja erro, será retornado um objeto que foi contruído pelo MulterError
@@ -27,8 +27,11 @@ class FotoController {
       }
       // precisamos desses campos que foram enviados na requisição ao enviar o arquivo
       const { originalname, filename } = req.file;
-      // criamos a foto
-      const foto = Foto.create({ originalname, filename });
+      // vamos enviar na requisição o aluno_id para identificar qual usuário será a foto
+      // então precisamos desse dado, lembrando que só podemos enviar para um aluno existente
+      const { aluno_id } = req.body;
+      // criamos a foto no banco de dados e enviamos os campos capturados
+      const foto = await Foto.create({ originalname, filename, aluno_id });
       // e retornamos
       return res.json(foto);
     });
